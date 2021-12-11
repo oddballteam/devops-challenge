d4226473f9e1c6e5daa7681c90f62e3a13c14f2c
john s-g <john.stevens-garmon@oddball.io>
2021-12-10 21:15:50 -0500

    remove nginx stuff - no longer using

--- terraform/app/ecs.tf
+++ terraform/app/ecs.tf
@@ -25,46 +25,10 @@ module "app_container" {
   }
 }
 
-#module "nginx_container" {
-#  source = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition.git?ref=tags/0.58.1"
-#  container_name               = var.nginx-name
-#  container_image              = var.container_image_nginx
-#  container_memory             = var.container_memory
-#  container_memory_reservation = var.container_memory_reservation
-#  container_cpu                = var.container_cpu
-#  container_depends_on = [
-#    {
-#      containerName = var.name
-#      condition     = "START"
-#    }
-#  ]
-#  port_mappings = [
-#    {
-#      containerPort = var.nginx_container_port
-#      hostPort      = var.nginx_container_port
-#      protocol      = "tcp"
-#    }
-#  ]
-#  log_configuration = {
-#    logDriver = var.log_driver
-#    options = {
-#      "awslogs-region"        = var.region
-#      "awslogs-group"         = aws_cloudwatch_log_group.nginx.name
-#      "awslogs-stream-prefix" = var.nginx-name
-#    }
-#    secretOptions = null
-#  }
-#}
-
 resource "aws_cloudwatch_log_group" "app" {
   name              = "/${var.namespace}/app"
   retention_in_days = 30
 }
-
-#resource "aws_cloudwatch_log_group" "nginx" {
-#  name = "/${var.namespace}/nginx"
-#  retention_in_days = 30
-#}
 
 resource "aws_ecs_cluster" "default" {
   name = "${local.prefix}-ecs-cluster"
