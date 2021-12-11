7fc04d6acf2bd47b1b7037c11b19929108a26d3c
john s-g <john.stevens-garmon@oddball.io>
2021-12-10 21:12:19 -0500

    add ecs service

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -18,9 +18,9 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  region = var.region
 }
 
 locals {
-  prefix            = "${var.namespace}-${var.name}-${var.stage}"
+  prefix = "${var.namespace}-${var.name}-${var.stage}"
 }
--- terraform/app/network.tf
+++ terraform/app/network.tf
@@ -1,3 +1,8 @@
+locals {
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnet_ids.us-east.ids
+}
+
 data "aws_vpc" "default" {
   filter {
     name   = "is-default"
@@ -6,7 +11,7 @@ data "aws_vpc" "default" {
 }
 
 data "aws_subnet_ids" "us-east" {
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = local.vpc_id
   filter {
     name   = "availability-zone"
     values = ["us-east-1a", "us-east-1b"]
